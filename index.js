f6c13bc49edb9bc2f5d839579560295ef14dfc0f
halleklum <halleklum@gmail.com>
2023-07-08 16:41:16 -0400

    initial commit, set up folders and files and started working on the index.js file.

---
+++ index.js
@@ -0,0 +1,59 @@
+// TODO: Include packages needed for this application
+const fs = require('fs')
+const inquirer = require('inquirer')
+const generateMarkdown = require('./utils/generateMarkdown')
+
+
+// TODO: Create an array of questions for user input
+const questions = [
+{
+    type: "input",
+    name: "title",
+    message: "What do you want the name of your Project Title to be?"
+}, {
+    type: "input",
+    name: "description",
+    message: "Please provide a short description of your project.",
+}, {
+    type: "input",
+    name: "installation",
+    message: "what are the installation instructions for your application?",
+}, {
+    type: "input",
+    name: "usage",
+    message: "How to you use your application?"
+}, {
+    type: "input",
+    name: "contribution",
+    message: "Please explain the guidelines for contributing to this project.",
+}, {
+    type: "input",
+    name: "test",
+    message: "How can people run the tests for your application?"
+}, {
+    type: "list",
+    name: "license",
+    message: "Which license would you like to use for this projcet?",
+    choices: ["GNU General Public License 3.0", "MIT", "Apache License 2.0", "Mozilla Public License 2.0", "none"],
+}, {
+    type: "input",
+    name: "GitHub",
+    message: "What is your GitHub username?",
+}, {
+    type: "input",
+    name: "Email",
+    message: "What is your email?",
+},
+
+
+
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
